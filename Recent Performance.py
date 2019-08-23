56ffef2ac9d540421b196e9589b4bcdc8e8e1e32
Dan Wang <siwu320@sjtu.edu.cn>
2019-08-23 14:07:17 +0800

    Add files via upload

--- Recent Performance.py
+++ Recent Performance.py
@@ -16,13 +16,13 @@ acc=0
 pp=0
 max_combomap=0
 max_comboyou=0
-time_span=386400
+time_span=86400
 print('在过去的',time_span,'秒内，你留了在BP100之内的以下成绩：')
 while item<100:
 	timeset=scores[item]['time']
 	timeArray=time.strptime(timeset,"%Y-%m-%dT%H:%M:%SZ")
 	timeStamp=int(time.mktime(timeArray))
-	if ticks-timeStamp<=time_span:
+	if ticks-timeStamp<=time_span+28800:
 		information=scores[item]['beatmap']['song_name']
 		acc=scores[item]['accuracy']
 		pp=scores[item]['pp']
